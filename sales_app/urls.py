b70c9e6dee657060e258129b0791e1bafb19c8bf
DipuSarker <mr.d.sarker@gmail.com>
2025-03-05 12:07:15 +0600

    Master on django session

--- sales_app/urls.py
+++ sales_app/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('setSessionData/', views.setSessionData, name='setSessionData'),
+    path('getSessionData/', views.getSessionData, name='getSessionData'),
+    path('clearSessionData/', views.clearSessionData, name='clearSessionData'),
+    path('allSessionData/', views.allSessionData, name='allSessionData'),
 ]
